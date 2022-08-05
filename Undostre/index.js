ec4a771ad17c996a3cd3effffb988d361a49d6de
santosh9yogi2 <81739647+santosh9yogi2@users.noreply.github.com>
2022-08-05 15:09:01 +0530

    Add files via upload

---
+++ Undostre/index.js
@@ -0,0 +1,208 @@
+
+function navlink(link_id){
+  if(link_id == 'navbar1'){
+    document.getElementById('phone_recharge').style.display = 'block'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar2'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'block'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar3'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'block'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar4'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'block'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar5'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'block'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar6'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'block'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar7'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'block'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar8'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'block'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar9'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'block'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar10'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'block'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar11'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'block'
+    document.getElementById('movie_ticket').style.display = 'none'
+  }
+  else if(link_id == 'navbar12'){
+    
+    alert("Santosh Yogi is  VIP")
+  }
+  else if(link_id == 'navbar13'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'block'
+  }
+  else if(link_id == 'navbar14'){
+    document.getElementById('phone_recharge').style.display = 'none'
+    document.getElementById('recharge_tag').style.display = 'none'
+    document.getElementById('electrical_pay').style.display = 'none'
+    document.getElementById('internet_pay').style.display = 'none'
+    document.getElementById('tv_pay').style.display = 'none'
+    document.getElementById('cell_pay').style.display = 'none'
+    document.getElementById('water_pay').style.display = 'none'
+    document.getElementById('gas_pay').style.display = 'none'
+    document.getElementById('entertainment_pay').style.display = 'none'
+    document.getElementById('urban_trans_pay').style.display = 'none'
+    document.getElementById('gift_card').style.display = 'none'
+    document.getElementById('movie_ticket').style.display = 'none'
+    
+    document.getElementById('loan_modal').style.display = 'block'
+
+  }
+  
+}
+function loan_close(){
+  document.getElementById('loan_modal').style.display = 'none'
+}
+
+function myModal(){
+    document.getElementById('myModal').style.display='block';
+}
+function modalClose() {
+   document.getElementById('myModal').style.display = 'none';
+  }
+  window.onclick = function(event) {
+    if (event.target == myModal) {
+        document.getElementById('modal-content').style.display = 'none';
+    }    
+  }
